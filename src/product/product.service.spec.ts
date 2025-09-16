3f9003675cf115e91f27afe0292159769ee8a420
Tomas Gambino <tomigambino21@gmail.com>
2025-09-16 17:17:51 -0300

    Corregi test de createProduct y agregue otro test donde se ingrese un tipo de producto invalido

--- src/product/product.service.spec.ts
+++ src/product/product.service.spec.ts
@@ -3,6 +3,7 @@ import { ProductService } from "./product.service"
 import { getRepositoryToken } from "@nestjs/typeorm"
 import { ProductEntity } from "src/common/entities/product.entity"
 import { ProductTypeService } from "src/productType/productType.service"
+import { NotFoundException } from "@nestjs/common"
 
 describe('productService', () => {
     let productService: ProductService
@@ -81,7 +82,7 @@ describe('productService', () => {
 
             // Configuramos los mocks
             mockProductTypeService.findProductTypeById.mockResolvedValue(mockProductType)
-            mockProductRepository.create.mockResolvedValue(mockCreatedProduct)
+            mockProductRepository.create.mockReturnValue(mockCreatedProduct)
             mockProductRepository.save.mockResolvedValue(mockSaveProduct)
 
             // Obtenemos el resultado del service (ACC - ACtuar)
@@ -90,6 +91,46 @@ describe('productService', () => {
             // Verificamos el resultado (ASSERT - Verificar)
             expect(result).toEqual(mockSaveProduct)
             expect(result.images).toEqual([]);
+            expect(mockProductTypeService.findProductTypeById)
+            .toHaveBeenCalledWith(createProductDto.productTypeId);
+            // Le sacamos el productTypeId del dto ya que en el create le pasamos directamente la entidad de productType
+            expect(mockProductRepository.create).toHaveBeenCalledWith(
+            expect.objectContaining({
+                name: createProductDto.name,
+                description: createProductDto.description,
+                price: createProductDto.price,
+                stock: createProductDto.stock,
+                isActive: createProductDto.isActive,
+                productType: mockProductType
+            })
+            );
+            expect(mockProductRepository.save)
+            .toHaveBeenCalledWith(mockCreatedProduct);
+        })
+        it('Crear producto con tipo de producto invalido', async () => {
+            // Definimos el dto que le vamos a pasar a la función createProduct()
+            const createProductDto = {
+                productTypeId: 2,
+                name: 'Pelota de UEFA Champions League',
+                description:'Pelota original usada en UEFA Champions League',
+                price: 3000,
+                stock: 10,
+                isActive: true
+            }
+
+            // Configuramos los mocks
+            mockProductTypeService.findProductTypeById.mockRejectedValue(new NotFoundException(`Product Type with ID ${createProductDto.productTypeId} not found`))
+
+
+            // Verificamos el resultado directamente aca, 
+            // porque si creamos una constante result el NotFoundException queda en esa linea y no llega al expect
+            await expect(productService.createProduct(createProductDto))
+            .rejects
+            .toThrow(NotFoundException);
+
+            // Verificamos que los metodos create y save no sean llamados
+            expect(mockProductRepository.create).not.toHaveBeenCalled();
+            expect(mockProductRepository.save).not.toHaveBeenCalled();
         })
     })
 })
